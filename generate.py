a62a2004dcce267f8c45873009b518cca98c40be
tkdrnjs0621 <tkdrnjs0621@snu.ac.kr>
2025-01-21 12:39:02 +0900

    initial commit

---
+++ generate.py
@@ -0,0 +1,121 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import argparse
+import re
+import time
+from datetime import timedelta
+from functools import partial
+import logging
+from datasets import load_dataset, Dataset
+from tqdm import tqdm
+import os
+from torch.utils.data import DataLoader
+
+from src.retriever import Retriever
+
+def generate(model, tokenizer, dataloader, log_every, **kwargs):
+    start = time.time()
+    output_ids = []
+    for i, inputs in tqdm(enumerate(dataloader, start=1),total=len(dataloader)):
+        inputs = inputs.to(model.device)
+        with torch.no_grad():
+            outputs = model.generate(**inputs, pad_token_id=tokenizer.eos_token_id, **kwargs)
+        output_ids.extend(outputs[:, inputs["input_ids"].size(1) :].tolist())
+        if i % log_every == 0:
+            end = time.time()
+            elapsed = end - start
+            total = elapsed * (len(dataloader) / i)
+    return output_ids
+
+def build_prompt(example, tokenizer):
+    prompt = tokenizer.apply_chat_template(example["messages"], tokenize=False, add_generation_prompt=True)
+    prompt_tokens = len(tokenizer(prompt, add_special_tokens=False).input_ids)
+    return {"prompt": prompt, "prompt_tokens": prompt_tokens}
+
+def collate_fn(batch, tokenizer):
+    prompt = [example["prompt"] for example in batch]
+    inputs = tokenizer(prompt, add_special_tokens=False, padding=True, return_tensors="pt")
+    return inputs
+
+def decode(example, tokenizer, feature):
+    text = tokenizer.decode(example[feature + "_ids"], skip_special_tokens=True)
+    return {feature: text}
+
+def map_question_to_input(row):
+    txt = row["question"]
+    messages = [{'role':'system','content':'You are a helpful assistant that answer to the question.'},{"role":'user',"content":txt}]
+    row["messages"]=messages
+    return row
+
+def map_entity(row):
+    pattern = r"\(([^,]+),\s*([^\)]+)\)"
+
+    ls = row["output"].strip().split('\n')
+    row["entities"]=[re.sub(pattern, r"\1 (\2)",l.strip()) for l in ls]
+    return row
+
+def set_file_handler(logger, path, level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"):
+    os.makedirs(os.path.dirname(path + "/run.log"), exist_ok=True)
+    handler = logging.FileHandler(path + "/run.log")
+    handler.setLevel(level)
+    formatter = logging.Formatter(format)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Full Run")
+
+    ### Dataset Configuration
+    parser.add_argument("--dataset_path", type=str, default="data/eval_data/triviaqa_test.jsonl", help="model name for evaluation")
+    
+    ### Saving Options
+    parser.add_argument("--model_path", type=str, default="meta-llama/Meta-Llama-3.1-8B-Instruct", help="path to local peft adapter (e.g. peft/sft/lora/Llama-3.1-8B-Instruct)")
+    parser.add_argument("--save_path", type=str, default="results/tqa_vanilla.jsonl", help="path to inference data to evaluate (e.g. inference/baseline/zero_v1/Llama-3.1-8B-Instruct)")
+    parser.add_argument("--num_proc", type=int, default=16, help="number of processors for processing datasets")
+    parser.add_argument("--log_every", type=int, default=20, help="logging interval in steps")
+    
+    ### Generation Options
+    parser.add_argument("--batch_size", type=int, default=16, help="batch size for inference")
+    parser.add_argument("--max_tokens", type=int, default=300, help="generation config; max new tokens")
+    parser.add_argument("--do_sample", type=bool, default=False, help="generation config; whether to do sampling, greedy if not set")
+    parser.add_argument("--temperature", type=float, default=0.0, help="generation config; temperature")
+    parser.add_argument("--top_k", type=int, default=50, help="generation config; top k")
+    parser.add_argument("--top_p", type=float, default=0.1, help="generation config; top p, nucleus sampling")
+
+    ### Retrieval Options
+    parser.add_argument("--retrieval_model_name_or_path", type=str, default="facebook/contriever-msmarco", help="path to directory containing model weights and config file")
+    parser.add_argument("--retrieval_embedding_size", type=int, default=768, help="path to directory containing model weights and config file")
+    parser.add_argument("--passages", type=str, default='data/corpus/psgs_w100.tsv', help="Path to passages (.tsv file)")
+    parser.add_argument("--passages_embeddings", type=str, default='data/corpus/wikipedia_embeddings/*', help="Glob path to encoded passages")
+    parser.add_argument("--indexing_batch_size", type=int, default=1000000, help="Batch size of the number of passages indexed")
+    parser.add_argument("--save_or_load_index", action="store_true", help="If enabled, save index and load index if it exists")
+    parser.add_argument("--lowercase", action="store_true", help="If enabled, save index and load index if it exists")
+    parser.add_argument("--normalize_text", action="store_true", help="If enabled, save index and load index if it exists")
+    parser.add_argument("--retrieval_n_subquantizers", type=int, default=0, help="Number of subquantizer used for vector quantization, if 0 flat index is used")
+    parser.add_argument("--retrieval_n_bits", type=int, default=8, help="Number of bits per subquantizer")
+    parser.add_argument("--per_gpu_batch_size", type=int, default=1000000, help="Number of bits per subquantizer")
+    parser.add_argument("--question_maxlength", type=int, default=100000, help="Number of bits per subquantizer")
+    parser.add_argument("--max_k", type=int, default=100, help="Number of documents to retrieve per questions")
+    
+    args = parser.parse_args()
+
+    retriever = Retriever(args)
+    retriever.setup_retriever()
+
+    dataset = Dataset.from_json(args.dataset_path)
+
+    model = AutoModelForCausalLM.from_pretrained(args.model_path, torch_dtype=torch.bfloat16, device_map="auto", attn_implementation="flash_attention_2")
+    tokenizer = AutoTokenizer.from_pretrained(model.config._name_or_path, padding_side="left")
+    tokenizer.pad_token = tokenizer.eos_token
+    model.eval()
+
+    dataset = dataset.map(partial(map_question_to_input))
+    dataset = dataset.map(partial(build_prompt, tokenizer=tokenizer),num_proc=args.num_proc)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_proc, collate_fn=partial(collate_fn, tokenizer=tokenizer), pin_memory=True) 
+    print("Length of DataLoader :",len(dataloader))
+
+    output_ids = generate(model, tokenizer, dataloader, args.log_every, max_new_tokens=args.max_tokens, do_sample=args.do_sample, temperature=args.temperature, top_k=args.top_k, top_p=args.top_p)
+    dataset = dataset.add_column("output_ids", output_ids) 
+    dataset = dataset.map(partial(decode, tokenizer=tokenizer, feature="output"), num_proc=args.num_proc)
+    dataset = dataset.map(map_entity)
+    dataset.to_json(args.save_path, lines=True)
---
+++ src/retriever.py
@@ -0,0 +1,140 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import argparse
+import json
+import pickle
+import time
+import glob
+from pathlib import Path
+
+import numpy as np
+import torch
+from transformers import AutoModel, AutoTokenizer
+
+import src.index
+import src.data
+import src.normalize_text
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+class Retriever:
+    def __init__(self, args, model=None, tokenizer=None) :
+        self.args = args
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def embed_queries(self, args, queries):
+        embeddings, batch_question = [], []
+        with torch.no_grad():
+            for k, q in enumerate(queries):
+                if args.lowercase:
+                    q = q.lower()
+                if args.normalize_text:
+                    q = src.normalize_text.normalize(q)
+                batch_question.append(q)
+
+                if len(batch_question) == args.per_gpu_batch_size or k == len(queries) - 1:
+
+                    encoded_batch = self.tokenizer.batch_encode_plus(
+                        batch_question,
+                        return_tensors="pt",
+                        max_length=args.question_maxlength,
+                        padding=True,
+                        truncation=True,
+                    )
+                    encoded_batch = {k: v.cuda() for k, v in encoded_batch.items()}
+                    output = self.model(**encoded_batch)
+
+                    last_hidden = output["last_hidden_state"]
+                    attention_mask = encoded_batch['attention_mask']
+                    last_hidden = last_hidden.masked_fill(~attention_mask[..., None].bool(), 0.0)
+
+                    emb = last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+                    embeddings.append(emb.cpu())
+
+        embeddings = torch.cat(embeddings, dim=0)
+        print(f"Questions embeddings shape: {embeddings.size()}")
+
+        return embeddings.numpy()
+    
+    def index_encoded_data(self, index, embedding_files, indexing_batch_size):
+        allids = []
+        allembeddings = np.array([])
+        for i, file_path in enumerate(embedding_files):
+            print(f"Loading file {file_path}")
+            with open(file_path, "rb") as fin:
+                ids, embeddings = pickle.load(fin)
+
+            allembeddings = np.vstack((allembeddings, embeddings)) if allembeddings.size else embeddings
+            allids.extend(ids)
+            while allembeddings.shape[0] > indexing_batch_size:
+                allembeddings, allids = self.add_embeddings(index, allembeddings, allids, indexing_batch_size)
+
+        while allembeddings.shape[0] > 0:
+            allembeddings, allids = self.add_embeddings(index, allembeddings, allids, indexing_batch_size)
+
+        print("Data indexing completed.")
+
+
+    def add_embeddings(self, index, embeddings, ids, indexing_batch_size):
+        end_idx = min(indexing_batch_size, embeddings.shape[0])
+        ids_toadd = ids[:end_idx]
+        embeddings_toadd = embeddings[:end_idx]
+        ids = ids[end_idx:]
+        embeddings = embeddings[end_idx:]
+        index.index_data(ids_toadd, embeddings_toadd)
+        return embeddings, ids
+
+    def add_passages(self, passages, top_passages_and_scores):
+        docs = [passages[doc_id] for doc_id in top_passages_and_scores[0][0]]
+        return docs
+
+    def setup_retriever(self):
+        print(f"Loading model from: {self.args.retrieval_model_name_or_path}")
+        self.model = AutoModel.from_pretrained(self.args.retrieval_model_name_or_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.args.retrieval_model_name_or_path)
+        self.model.eval()
+        self.model = self.model.cuda()
+        # if not self.args.no_fp16:
+        #     self.model = self.model.half()
+
+        self.index = src.index.Indexer(self.args.retrieval_embedding_size, self.args.retrieval_n_subquantizers, self.args.retrieval_n_bits)
+
+        # index all passages
+        input_paths = glob.glob(self.args.passages_embeddings)
+        input_paths = sorted(input_paths)
+        embeddings_dir = os.path.dirname(input_paths[0])
+        index_path = os.path.join(embeddings_dir, "index.faiss")
+        if self.args.save_or_load_index and os.path.exists(index_path):
+            self.index.deserialize_from(embeddings_dir)
+        else:
+            print(f"Indexing passages from files {input_paths}")
+            start_time_indexing = time.time()
+            self.index_encoded_data(self.index, input_paths, self.args.indexing_batch_size)
+            print(f"Indexing time: {time.time()-start_time_indexing:.1f} s.")
+            if self.args.save_or_load_index:
+                self.index.serialize(embeddings_dir)
+
+        # load passages
+        print("loading passages")
+        self.passages = src.data.load_passages(self.args.passages)
+        self.passage_id_map = {x["id"]: x for x in self.passages}
+        print("passages have been loaded")
+
+    def search_document(self, query, top_n=10):
+        questions_embedding = self.embed_queries(self.args, [query])
+
+        # get top k results
+        start_time_retrieval = time.time()
+        top_ids_and_scores = self.index.search_knn(questions_embedding, self.args.max_k)
+        print(f"Search time: {time.time()-start_time_retrieval:.1f} s.")
+
+        return self.add_passages(self.passage_id_map, top_ids_and_scores)[:top_n]
+    
+    def reset_args(self,args):
+        self.args = args
